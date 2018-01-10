d04fc4666414e9ad8fc6bb475f0686c3c87a73fb
ruoyuan <ruoruolove@gmail.com>
2018-01-09 21:39:54 -0500

    notes

--- Wk2_L2_add.R
+++ Wk2_L2_add.R
@@ -1,1 +1,15 @@
-print(2+3)
+
+
+
+(contraception
+  %>%group_by(age,urban)
+  %>%summarise(
+    prop=mean(as.numeric(use)-1),
+    n=n(),
+    se=sqrt(prop*(1-prop)/n))
+)->contr.sum
+
+ggplot(contr_sum,aes(age,prop,colour=urban))+
+  geom+point(aes(size=1))+
+  geom_linerange(aes(ymin=prop-se,ymax=prop+se))+
+  scale_colour_brewer(palette="Dark2")
