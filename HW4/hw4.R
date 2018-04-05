2f22b4534ecb4b995a3f1291260f7c5ae7912562
Monica <lir32@mcmaster.ca>
2018-04-05 01:10:12 -0400

    hw6

--- HW4/hw4.R
+++ HW4/hw4.R
@@ -3,14 +3,18 @@ library(reshape2)
 library(tidyverse)
 library(directlabels)
 
-fish_data<-read_csv("fish.csv",col_names = TRUE)
-colnames(fish_data)[3]<-"SampleID"
-fish_name<-read_csv("fish_names.csv",col_names = TRUE)
+fish_data<-read_csv("fish.csv")
+names(fish_data)[3]<-make.names("SampleID")
+
+
+fish_name<-read_csv("fish_names.csv")
 
 #wide format to long format
 fish_long<-melt(fish_data, id.vars = c("SampleID", "Site","MetCode","SamplerType"),
      variable.name = "treatment_variable", 
      value.name = "treatment_value")
+
+
 
 #merge name data with  long format data.
 fish_name$treatment_variable<- fish_name$abbr
@@ -18,17 +22,34 @@ full<-merge(fish_long, fish_name)
 (full%>%select(-abbr))->full
 
 
+(full%>%group_by(Site,drugcat)
+  %>%summarise(n=n(),avg=mean(treatment_value))
+)->a
 
+p11<-(ggplot(a,aes(x=Site,y=avg,colour=treatment_variable))
+     +geom_point(alpha=0.3)
+     +facet_wrap(~drugcat,labeller = label_both,scales = "free"))
+
+pp11<-(p1+geom_line(aes(group=treatment_variable))
+      +geom_dl(aes(label=treatment_variable),,method="last.points")
+      +theme(axis.text.x = element_text(angle = 60))
+      +theme(legend.position="none")
+)
+print(pp11)
 
 p1<-(ggplot(full,aes(x=Site,y=treatment_value,colour=treatment_variable))
      +geom_point(alpha=0.3)
      +facet_wrap(~drugcat,labeller = label_both,scales = "free"))
 
-print(p1+geom_line(aes(group=treatment_variable))
+pp1<-(p1+geom_line(aes(group=treatment_variable))
    +geom_dl(aes(label=treatment_variable),,method="last.points")
    +theme(axis.text.x = element_text(angle = 60))
    +theme(legend.position="none")
 )
+
+
+
+ggplotly(pp1)
 
 p2<-ggplot(full,aes(x=treatment_variable,y=treatment_value,colour=Site))
 print(p2+geom_line(aes(group=Site))+facet_wrap(~Site,labeller = label_both,scales = "free")
